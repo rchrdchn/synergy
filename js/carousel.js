790abaa3e85cb0000e496cfb98e192fa221ce7d0
Richard Chan <richardchanme@gmail.com>
2017-05-20 18:31:07 -0700

    logo section added

---
+++ js/carousel.js
@@ -0,0 +1,25 @@
+// http://kenwheeler.github.io/slick/
+// https://www.solodev.com/blog/web-design/adding-an-infinite-client-logo-carousel-to-your-website.stml
+
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: false,
+        pauseOnHover: true,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
